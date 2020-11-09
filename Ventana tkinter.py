354bfc9bd30d729fc6146db02d31d68012a42db6
sebastianflorezalarcon <73863222+sebastianflorezalarcon@users.noreply.github.com>
2020-11-08 19:24:32 -0500

    Creacion de la ventana tkinter

---
+++ Ventana tkinter.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  8 19:11:39 2020
+
+@author: USER
+"""
+
+from tkinter import *
+
+if __name__ == "__main__":
+
+    ventana = Tk()
+
+
+    ventana.configure(background="light blue")
+
+    ventana.title("Calculadora Proyecto")
+
+    ventana.geometry("300x300")
+
+    ventana.mainloop()
